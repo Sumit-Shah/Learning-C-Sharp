b100cadb2157dafb3814eb518398969b8a3995f4
Sumit-Shah <sumitshah001122@gmail.com>
2024-09-18 21:52:29 +0545

    Covered Basics

---
+++ Learning-C-Sharp/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hellow World!");
+
+            //int x = 1;
+            //int y = 2;
+            //int z = 3;
+
+            //int x = 10,
+            //    y = 20,
+            //    z = 30;
+
+            //int age = 24;
+            //Console.WriteLine(age);
+
+
+            //long bigNumber = -9000000L;
+
+            //Console.WriteLine(bigNumber);
+
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+
+            //Console.WriteLine(long.MinValue);
+            //Console.WriteLine(long.MaxValue);
+
+
+            //double nagative = -104.25D;
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(double.MinValue);
+
+
+            //float precicion = 5.00001F;
+            //Console.WriteLine(precicion);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(float.MinValue);
+
+
+            //decimal money = 14.99M;
+            //Console.WriteLine(money);
+            //Console.WriteLine(decimal.MaxValue);
+            //Console.WriteLine(decimal.MinValue);
+
+
+
+
+
+
+
+            //STRINGS
+            //string name = "";
+            //char letter = 'A';
+
+            //Console.Write("Your name is ");
+            //Console.WriteLine(name);
+
+            //Console.WriteLine();
+            //Console.Write(letter);
+            //Console.ReadLine();
+
+
+
+
+
+            //CONVERTING STRING TO NNUMBERS
+
+            //String textAge = "24";
+            //int age = Convert.ToInt32(textAge);
+            //Console.WriteLine(age);
+
+
+            //string textBigNumber = "-9000000";
+            //long bigNumber = Convert.ToInt64(textBigNumber);
+            //Console.WriteLine(bigNumber);
+
+
+            //string textnegative = "-55.2";
+            //double nagative = Convert.ToDouble(textnegative);
+            //Console.WriteLine(nagative);
+
+
+            //string textPrecision = "5.000001";
+            //float precicion = Convert.ToSingle(textPrecision);
+            //Console.WriteLine(precicion);
+
+
+            //string textMoney = "14.99";
+            //decimal money = Convert.ToDecimal(textMoney);
+            //Console.WriteLine(money);
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+            //Boolean DATA TYPE
+            //bool value = true;
+
+            //bool isMale = true;
+            //Console.WriteLine(isMale);
+
+            //isMale = false;
+            //Console.WriteLine(isMale);
+
+            //Console.WriteLine();
+
+
+
+
+
+
+
+            //OPERATORS(+ - * /)
+            //int age = 23;
+            //double age = 23;
+
+            ////age++;
+            //age /= 10;
+            //Console.WriteLine(age);
+
+
+            ////age--
+            //age -= 10;
+            //Console.WriteLine(age);
+
+
+            //string name = "Ama";
+            //name += " Buba";
+            //Console.WriteLine(name);
+
+
+
+            //char ch = 'a';
+            //ch += 'b';
+            //Console.WriteLine(ch);
+
+
+
+            //int i = 0;
+            //Console.WriteLine(i++);
+            //Console.WriteLine(i);
+
+            //Console.WriteLine(++i);
+            //Console.WriteLine(i);
+
+
+
+            //REMINDER
+            int firstNum = 10;
+            int secondNum = 3;
+
+            //10 / 3 = 3
+            //3*3 = 9, 1 left over to get to 10
+            //10 % 2 = 0
+            //11 % 2 = 1
+            //12 % 2 = 0
+            //13 % 2 = 1
+
+            Console.WriteLine(firstNum % secondNum);
+            Console.WriteLine(100 % 90);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
